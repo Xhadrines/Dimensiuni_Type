8f73f148bb73d35e3f691233b8be8ce606660762
Xhadrines <95206340+Xhadrines@users.noreply.github.com>
2023-05-13 13:22:06 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,56 +18,56 @@ int main()
 {
 	cout << "=========================================" << endl;
 	
-    cout << "Informatii folosind sizeof: " << endl;
+    	cout << "Informatii folosind sizeof: " << endl;
 	
-    cout << "\tchar: " << sizeof(char) << " bytes." << endl;
+    	cout << "\tchar: " << sizeof(char) << " bytes." << endl;
 	
-    cout << "\tint : " << sizeof(int) << " bytes." << endl;
+    	cout << "\tint : " << sizeof(int) << " bytes." << endl;
 	
-    cout << "\tunsigned int: " << sizeof(unsigned int) << " bytes." << endl;
+    	cout << "\tunsigned int: " << sizeof(unsigned int) << " bytes." << endl;
 	
-    cout << "\tshort: " << sizeof(short) << " bytes." << endl;
+   	cout << "\tshort: " << sizeof(short) << " bytes." << endl;
 	
-    cout << "\tlong: " << sizeof(long) << " bytes." << endl;
+   	cout << "\tlong: " << sizeof(long) << " bytes." << endl;
 	
-    cout << "\tlong long: " << sizeof(long long) << " bytes." << endl;
+    	cout << "\tlong long: " << sizeof(long long) << " bytes." << endl;
 
-    cout << "\tfloat: " << sizeof(float) << " bytes." << endl;
+    	cout << "\tfloat: " << sizeof(float) << " bytes." << endl;
 	
-    cout << "\tdouble: " << sizeof(double) << " bytes." << endl;
+    	cout << "\tdouble: " << sizeof(double) << " bytes." << endl;
 	
-    cout << "\tlong double: " << sizeof(long double) << " bytes." << endl;
+    	cout << "\tlong double: " << sizeof(long double) << " bytes." << endl;
 
 	cout << "=========================================" << endl;
 	
 	cout << "Informatii folosind valorile din climits." << endl;
     
-    cout << "Valorile minime: " << endl;
+    	cout << "Valorile minime: " << endl;
 	
-    cout << "\tchar: " << CHAR_MIN << endl;
+    	cout << "\tchar: " << CHAR_MIN << endl;
 	
-    cout << "\tint: " << INT_MIN << endl;
+    	cout << "\tint: " << INT_MIN << endl;
 	
-    cout << "\tshort: " << SHRT_MIN << endl;
+    	cout << "\tshort: " << SHRT_MIN << endl;
 	
-    cout << "\tlong: " << LONG_MIN << endl; 
+    	cout << "\tlong: " << LONG_MIN << endl; 
 	
-    cout << "\tlong long: " << LLONG_MIN << endl;
+    	cout << "\tlong long: " << LLONG_MIN << endl;
     
-    cout << "Valorile maxime: " << endl;
+    	cout << "Valorile maxime: " << endl;
 	
-    cout << "\tchar: " << CHAR_MAX << endl;
+    	cout << "\tchar: " << CHAR_MAX << endl;
 	
-    cout << "\tint: " << INT_MAX << endl;
+    	cout << "\tint: " << INT_MAX << endl;
 	
-    cout << "\tshort: " << SHRT_MAX << endl;
+    	cout << "\tshort: " << SHRT_MAX << endl;
 	
-    cout << "\tlong: " << LONG_MAX << endl;
+    	cout << "\tlong: " << LONG_MAX << endl;
 	
-    cout << "\tlong long: " << LLONG_MAX << endl;
+    	cout << "\tlong long: " << LLONG_MAX << endl;
 	
 	cout << "=========================================" << endl;
 	
-    return 0;
+    	return 0;
 }
 
